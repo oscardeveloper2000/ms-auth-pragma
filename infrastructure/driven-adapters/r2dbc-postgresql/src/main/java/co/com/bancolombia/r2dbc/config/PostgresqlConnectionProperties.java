0c64724a8107129122ee933a1211600881f35567
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-08-23 09:26:07 -0500

    fix(PostgresqlConneciotion): add decorator ConfigurationProperties

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/config/PostgresqlConnectionProperties.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/config/PostgresqlConnectionProperties.java
@@ -3,7 +3,9 @@ package co.com.bancolombia.r2dbc.config;
 // TODO: Load properties from the application.yaml file or from secrets manager
 // import org.springframework.boot.context.properties.ConfigurationProperties;
 
-// @ConfigurationProperties(prefix = "adapters.r2dbc")
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "adapters.r2dbc")
 public record PostgresqlConnectionProperties(
         String host,
         Integer port,
