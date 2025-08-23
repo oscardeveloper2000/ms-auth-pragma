b24efc0313935df76e8c12615993fcf4be619d44
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-08-22 21:17:24 -0500

    feat(domain): add model and gateway of user

---
+++ domain/model/src/main/java/co/com/bancolombia/model/user/User.java
@@ -0,0 +1,25 @@
+package co.com.bancolombia.model.user;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+    private String email;
+    private Double baseSalary;
+
+}
