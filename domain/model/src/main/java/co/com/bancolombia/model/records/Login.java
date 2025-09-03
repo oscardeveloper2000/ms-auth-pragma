db302eaf894a7a6d9bc30f52db1448006ba82266
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-09-03 01:18:07 -0500

    feat(auth): add Login and Token records for authentication data handling

---
+++ domain/model/src/main/java/co/com/bancolombia/model/records/Login.java
@@ -0,0 +1,8 @@
+package co.com.bancolombia.model.records;
+
+/**
+ * Represents the data required to log in in the system.
+ */
+public record Login(String email, String password) {
+
+}
---
+++ domain/model/src/main/java/co/com/bancolombia/model/records/Token.java
@@ -0,0 +1,10 @@
+package co.com.bancolombia.model.records;
+
+/**
+ * Represents an authentication token.
+ *
+ * @param token the token string
+ */
+public record Token(String token) {
+
+}
